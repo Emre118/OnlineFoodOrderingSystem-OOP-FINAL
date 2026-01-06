230c0b54be9a96f5ebf5464c45dfe885bc57fc4b
Emre118 <yunusemreduran02@gmail.com>
2026-01-06 04:54:14 +0300

    Seed 7 restaurants and print restaurant list

--- src/app/ConsoleApp.java
+++ src/app/ConsoleApp.java
@@ -1,32 +1,62 @@
 package app;
 
-import model.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.MenuItem;
+import model.Restaurant;
 
 public class ConsoleApp {
 
     public static void main(String[] args) {
 
-        // Create restaurant and menu
-        Restaurant restaurant = new Restaurant("Chatto Burger");
+        List<Restaurant> restaurants = seedRestaurants();
 
-        MenuItem burger = new MenuItem("Burger", 120);
-        MenuItem cola = new MenuItem("Cola", 30);
+        System.out.println("=== Restaurants ===");
+        for (int i = 0; i < restaurants.size(); i++) {
+            System.out.println((i + 1) + ") " + restaurants.get(i).getName());
+        }
+    }
 
-        restaurant.addMenuItem(burger);
-        restaurant.addMenuItem(cola);
+    private static List<Restaurant> seedRestaurants() {
+        List<Restaurant> restaurants = new ArrayList<>();
 
-        // Create customer
-        Customer customer = new Customer("emre", "1234");
+        Restaurant r1 = new Restaurant("Chatto Burger");
+        r1.addMenuItem(new MenuItem("Burger", 120));
+        r1.addMenuItem(new MenuItem("Cola", 30));
 
-        // Create order and add items
-        Order order = new Order();
-        order.addItem(burger);
-        order.addItem(cola);
+        Restaurant r2 = new Restaurant("Pizza Lab");
+        r2.addMenuItem(new MenuItem("Margherita", 140));
+        r2.addMenuItem(new MenuItem("Ayran", 20));
 
-        // Print order summary
-        System.out.println("Order total: " + order.calculateTotal() + " TL");
+        Restaurant r3 = new Restaurant("Sushi Go");
+        r3.addMenuItem(new MenuItem("Sushi Roll", 220));
+        r3.addMenuItem(new MenuItem("Green Tea", 35));
 
-        order.markPaid();
-        System.out.println("Order status: " + order.getStatus());
+        Restaurant r4 = new Restaurant("Kebab House");
+        r4.addMenuItem(new MenuItem("Adana", 180));
+        r4.addMenuItem(new MenuItem("Salgam", 25));
+
+        Restaurant r5 = new Restaurant("Pasta Corner");
+        r5.addMenuItem(new MenuItem("Spaghetti", 160));
+        r5.addMenuItem(new MenuItem("Water", 10));
+
+        Restaurant r6 = new Restaurant("Coffee & More");
+        r6.addMenuItem(new MenuItem("Latte", 80));
+        r6.addMenuItem(new MenuItem("Cheesecake", 110));
+
+        Restaurant r7 = new Restaurant("Healthy Bowl");
+        r7.addMenuItem(new MenuItem("Chicken Bowl", 170));
+        r7.addMenuItem(new MenuItem("Smoothie", 90));
+
+        restaurants.add(r1);
+        restaurants.add(r2);
+        restaurants.add(r3);
+        restaurants.add(r4);
+        restaurants.add(r5);
+        restaurants.add(r6);
+        restaurants.add(r7);
+
+        return restaurants;
     }
 }
