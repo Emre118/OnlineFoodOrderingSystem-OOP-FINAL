decd33c5d6280d916daf8eba795491a13476d4b4
Emre118 <yunusemreduran02@gmail.com>
2026-01-06 08:00:21 +0300

    Add unit tests for Order item addition and total calculation

--- test/model/OrderTest.java
+++ test/model/OrderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class OrderTest {
 
+    // Test 1: Yeni siparişin başlangıç durumu
     @Test
     void newOrder_shouldBeCreatedWithZeroItems() {
         Order order = new Order();
@@ -13,5 +14,27 @@ public class OrderTest {
         assertEquals(0, order.getItemCount());
         assertEquals(0.0, order.calculateTotal());
         assertEquals(OrderStatus.CREATED, order.getStatus());
+    }
+
+    // Test 2: Ürün eklenince item sayısı artmalı
+    @Test
+    void addItem_shouldIncreaseItemCount() {
+        Order order = new Order();
+        MenuItem burger = new MenuItem("Burger", 100);
+
+        order.addItem(burger);
+
+        assertEquals(1, order.getItemCount());
+    }
+
+    // Test 3: Toplam fiyat doğru hesaplanmalı
+    @Test
+    void calculateTotal_shouldSumItemPrices() {
+        Order order = new Order();
+
+        order.addItem(new MenuItem("Burger", 100));
+        order.addItem(new MenuItem("Cola", 30));
+
+        assertEquals(130.0, order.calculateTotal());
     }
 }
